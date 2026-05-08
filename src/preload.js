eff3d91b0cb935c3838068a8380a7382b692bd42
Dinzeyi2 <133053311+Dinzeyi2@users.noreply.github.com>
2026-05-08 19:55:24 -0400

    Update preload.js

--- src/preload.js
+++ src/preload.js
@@ -1,16 +1,11 @@
-/**
- * Astra Desktop Preload
- * Exposes IPC bridge to settings UI safely.
- */
+'use strict';
 const { contextBridge, ipcRenderer } = require('electron');
-
 contextBridge.exposeInMainWorld('astra', {
-  getStats:     () => ipcRenderer.invoke('get-stats'),
-  getSettings:  () => ipcRenderer.invoke('get-settings'),
-  saveSettings: (s) => ipcRenderer.invoke('save-settings', s),
-  toggleProxy:  (e) => ipcRenderer.invoke('toggle-proxy', e),
+  getStats:    () => ipcRenderer.invoke('get-stats'),
+  getSettings: () => ipcRenderer.invoke('get-settings'),
+  saveSettings: s => ipcRenderer.invoke('save-settings', s),
+  toggleProxy:  e => ipcRenderer.invoke('toggle-proxy', e),
+  installCert:  () => ipcRenderer.invoke('install-cert'),
   resetStats:   () => ipcRenderer.invoke('reset-stats'),
   getAuditLog:  () => ipcRenderer.invoke('get-audit-log'),
-  certStatus:   () => ipcRenderer.invoke('cert-status'),
-  removeCert:   () => ipcRenderer.invoke('remove-cert'),
 });
